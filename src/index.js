0bba08950f10decdfc9120a5be66e6255c13f363
tanvir <tanvir.hasan@codemarshal.com>
2020-05-19 13:39:30 +0600

    feat(app): init forntend

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import { Route, Switch, BrowserRouter as Router } from 'react-router-dom';
+import App from './App';
+import Settings from './services/settings';
+import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <App/>
+        </Route>
+        <Route exact path="/settings">
+          <Settings/>
+        </Route>
+      </Switch>
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
---
+++ src/services/settings/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Settings = props => {
+  return (
+    <React.Fragment>
+      <h1>Settings</h1>
+    </React.Fragment>
+  );
+};
+
+export default Settings;
