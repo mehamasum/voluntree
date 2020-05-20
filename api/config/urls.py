bd1fc2c91ee4f7c278610ed28334f1c033a97906
tanvir <tanvir.hasan@codemarshal.com>
2020-05-20 14:21:48 +0600

    feat(app): add drf base

--- api/config/urls.py
+++ api/config/urls.py
@@ -14,8 +14,32 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf import settings
 from django.urls import path
+from django.conf.urls import include
+from rest_framework.routers import DefaultRouter, SimpleRouter
+from rest_framework.schemas import get_schema_view
+from rest_framework.documentation import include_docs_urls
+from voluntree.views import VoluntreeApiListView
+from .router import DefaultRouterWithAPIViews
+
+router = SimpleRouter()
+
+schema_view = get_schema_view(title=settings.API_BROWSER_HEADER, public=True)
+doc_urls = include_docs_urls(title=settings.API_BROWSER_HEADER)
+api_browser_urls = include('rest_framework.urls')
+voluntree_api_urls = include(('voluntree.urls', 'voluntree'), 'voluntree')
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('api/', doc_urls),
+    path('api/schema/', schema_view),
+    path('api/browser/', api_browser_urls),
+    path('api/admin/', admin.site.urls),
+    path('api/voluntree/', voluntree_api_urls),
 ]
+
+urlpatterns += router.urls
+
+root_router = DefaultRouterWithAPIViews()
+root_router.register('api/voluntree', VoluntreeApiListView, 'test')
+urlpatterns += root_router.urls
