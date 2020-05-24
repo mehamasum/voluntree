45610b036d76ba8c034ad1d7127b6ad28d7e5dcc
Masum <mehedi.masum@newscred.com>
2020-05-24 15:24:33 +0600

    Adapt ml pipeline to webhook format

--- api/voluntree/ml/test.py
+++ api/voluntree/ml/test.py
@@ -1,9 +1,40 @@
-from pipeline import preprocess, model_run
+from pipeline import pipleline
 
 if __name__ == '__main__':
-    r = preprocess('Nice! I\'m interested. See you then!')
-    model_run(r)
+
+    hook_payloads= [
+        {
+            "field": "feed",
+            "value": {
+                "item": "comment",
+                "comment_id": "44444444_4444444441",
+                "verb": "add",
+                "published": 1,
+                "created_time": 1590054191,
+                "message": "Nice! I'm interested. See you then!",
+                "from": {
+                "name": "User",
+                "id": "1067280970047460"
+                }
+            }
+        },
+        {
+            "field": "feed",
+            "value": {
+                "item": "comment",
+                "comment_id": "44444444_4444444442",
+                "verb": "add",
+                "published": 1,
+                "created_time": 1590054191,
+                "message": "I can't come but still want to help. Do you accept donations on credit card?",
+                "from": {
+                "name": "User",
+                "id": "1067280970047460"
+                }
+            }
+        }
+    ]
 
 
-    r = preprocess('I can\'t come but still want to help. Do you accept donations on credit card?')
-    model_run(r)
+    for payload in hook_payloads:
+        pipleline(payload) 
