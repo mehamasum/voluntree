b988091a214eccf2a1ab3b71bac56d721685e9fb
tanvir <tanvir.hasan@codemarshal.com>
2020-05-24 17:35:20 +0600

    feat(api): auto webhook triger for new comment

--- api/schedule_task/tasks.py
+++ api/schedule_task/tasks.py
@@ -1,3 +1,4 @@
+import json
 import requests
 import logging
 from config.celery import app
@@ -8,7 +9,9 @@ from .services import FacebookWebHookService
 def fetch_comment():
     logging.debug(f"Running comment fetch task")
     url = 'http://localhost:8000/api/voluntree/facebook/webhook:page/'
+    headers = {'content-type': "application/json"}
     new_comments = FacebookWebHookService.get_new_comments()
+    print("new_comments", new_comments)
     for comment in new_comments:
-        print("comment", comment)
+        requests.post(url, headers=headers, data=json.dumps(comment))
     return new_comments
