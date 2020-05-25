7ace00f789059d1f90173a88865e404a2b36fb2c
shakil ahmed <shakil.austcse27@gmail.com>
2020-05-25 21:46:09 +0600

    feat(api): django channel backend

--- api/voluntree/consumers.py
+++ api/voluntree/consumers.py
@@ -16,6 +16,17 @@ class VolunteerInterestConsumer(AsyncWebsocketConsumer):
 
         await self.accept()
 
+        # return the first response 
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'generate_response',
+                'from_created_at': None,
+                'limit': 20,
+                'interested': True,
+            }
+        )
+
     async def disconnect(self, close_code):
         # Leave room group
         await self.channel_layer.group_discard(
@@ -24,24 +35,29 @@ class VolunteerInterestConsumer(AsyncWebsocketConsumer):
         )
 
     # Receive message from WebSocket
-    async def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        message = text_data_json['message']
-
-        # Send message to room group
+    async def receive(self, query_data):
+        query_data_json = json.loads(query_data)
+        from_created_at = query_data_json.get('from_created_at', None)
+        limit = query_data_json.get('limit', 20)
+        interested = query_data_json.get('interested', True)
         await self.channel_layer.group_send(
             self.room_group_name,
             {
-                'type': 'chat_message',
-                'message': message
+                'type': 'generate_response',
+                'from_created_at': from_created_at,
+                'limit': limit,
+                'interested': interested
             }
         )
 
     # Receive message from room group
-    async def chat_message(self, event):
-        message = event['message']
+    async def generate_response(self, event):
+        from_created_at = event.get('from_created_at')
+        limit = event.get('limit', 20)
+        interested = event.get('interested')
+        queryset = Interest.objects.filter(
+            create_at__lt=from_created_at,interested=interested)[:limit]
+        serializer = InterestGeterializer(queryset, many=True)
+        response = InterestGeterializer()
 
-        # Send message to WebSocket
-        await self.send(text_data=json.dumps({
-            'message': message
-        }))
+        await self.send(response)
