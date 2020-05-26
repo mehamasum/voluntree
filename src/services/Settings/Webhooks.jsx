0f7026d13de75e0259d5e66d736ba54d25c7d529
Masum <mehedi.masum@newscred.com>
2020-05-27 02:02:57 +0600

    Add webhook ux

---
+++ src/services/Settings/Webhooks.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import {Form, Input, Button, Radio, Typography, Card} from 'antd';
+
+const FormLayoutDemo = () => {
+    const [form] = Form.useForm();
+    return (
+        <div>
+            <div>
+                <Typography.Text type="secondary">To receive messages and other events sent by Messenger users, the app should enable webhooks integration.</Typography.Text>
+            </div>
+            <br/>
+            <Form
+                layout={'vertical'}
+                form={form}
+                initialValues={{
+                    layout: 'vertical',
+                }}
+            >
+                <Form.Item label="Callback URL">
+                    <Input
+                        placeholder="input placeholder"
+                        value="https://50be959a.ngrok.io/api/voluntree/facebook/webhook:messenger/"
+                        onChange={()=>{}}
+                    />
+                </Form.Item>
+                <Form.Item label="Verify Token">
+                    <Input
+                        placeholder="input placeholder"
+                        value="super-secret"
+                        type="password"
+                        onChange={()=>{}}
+                    />
+                </Form.Item>
+            </Form>
+        </div>
+    );
+};
+
+export default FormLayoutDemo;
