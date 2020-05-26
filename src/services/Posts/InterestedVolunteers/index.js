d004a54fef4075b3e22815fa3b47ee2e8592d1dc
Masum <mehedi.masum@newscred.com>
2020-05-27 04:45:41 +0600

    Post details view

--- src/services/Posts/InterestedVolunteers/index.js
+++ src/services/Posts/InterestedVolunteers/index.js
@@ -1,80 +1,82 @@
-import React, { useMemo, useState, useEffect, useCallback} from 'react';
-import { List, Card, Button, Space } from 'antd';
-import { useFetch } from '../../../hooks';
+import React, {useMemo, useState, useEffect, useCallback} from 'react';
+import {List, Card, Button, Space, Avatar, Typography} from 'antd';
+import {useFetch} from '../../../hooks';
 import InfiniteScroll from 'react-infinite-scroller';
 
 import './styles.css';
 
 const InterestedVolunteers = props => {
-  const {id} = props;
-  const host = process.env.REACT_APP_BACKEND_HOST;
-  const [interest_response,, setUrl,,,is_loading] = useFetch(`/api/voluntree/posts/${id}/interests/`);
-  const [listData, setListData] = useState([]);
-  const [nextUrl, setNextUrl] = useState(null);
+    const {id} = props;
+    const host = process.env.REACT_APP_BACKEND_HOST;
+    const [interest_response, , setUrl, , , is_loading] = useFetch(`/api/voluntree/posts/${id}/interests/`);
+    const [listData, setListData] = useState([]);
+    const [nextUrl, setNextUrl] = useState(null);
 
-   
-  const onMessage = useCallback((e) => {
-      
-    let json_parsed_data = JSON.parse(e.data);
-    let data = json_parsed_data.data
-    if( data.status == 201 ) { // newly created instance 
-      setListData(prevListData => ([data.response, ...prevListData]));
-    }
-  }, [listData]);
- 
-  useEffect(()=> {
-    const endPoint = 'ws://localhost:8000' + `/ws/volunteers/${id}/`;
-    const ws = new WebSocket(endPoint);
-    ws.onerror = (e) => {
-      console.log('error', e);
-    }
-    
-    ws.onmessage = onMessage;
-  
-    ws.onopen = (e) => {
-      console.log('onopen', e);
-    }
-  
-    ws.onclose = (e) => {
-      console.log('onclose', e);
-    }
 
-  },[]);
+    const onMessage = useCallback((e) => {
 
-  useEffect(() => {
-    if(interest_response) {
-      setListData(prevListData => ([...prevListData, ...interest_response.results]));
-      setNextUrl(interest_response.next);
-    }
-  }, [interest_response && interest_response.results])
+        let json_parsed_data = JSON.parse(e.data);
+        let data = json_parsed_data.data
+        if (data.status == 201) { // newly created instance
+            setListData(prevListData => ([data.response, ...prevListData]));
+        }
+    }, [listData]);
 
-  return (
-    <React.Fragment>
-      <InfiniteScroll
-          initialLoad={false}
-          pageStart={0}
-          loadMore={() => nextUrl && setUrl(nextUrl.replace(host, ''))}
-          hasMore={!is_loading && (nextUrl ? true : false) }
-          loader={<div className="loader" key={0}>Loading ...</div>}
-          useWindow={false}
-        >
-        <List
-        header={<div style={{
-          textAlign: 'center',
-          height: 32,
-          lineHeight: '32px',
-        }}><h3>Interested Volunteers</h3></div>}
-        bordered
-        dataSource={listData}
-        renderItem={item => (
-          <List.Item>
-              <div>{item.volunteer.facebook_user_id}</div>
-          </List.Item>
-        )}
-      />
-      </InfiniteScroll>
-    </React.Fragment>
-  );
+    useEffect(() => {
+        const endPoint = 'ws://localhost:8000' + `/ws/volunteers/${id}/`;
+        const ws = new WebSocket(endPoint);
+        ws.onerror = (e) => {
+            console.log('error', e);
+        }
+
+        ws.onmessage = onMessage;
+
+        ws.onopen = (e) => {
+            console.log('onopen', e);
+        }
+
+        ws.onclose = (e) => {
+            console.log('onclose', e);
+        }
+
+    }, []);
+
+    useEffect(() => {
+        if (interest_response) {
+            setListData(prevListData => ([...prevListData, ...interest_response.results]));
+            setNextUrl(interest_response.next);
+        }
+    }, [interest_response && interest_response.results])
+
+    return (
+        <React.Fragment>
+            <InfiniteScroll
+                initialLoad={false}
+                pageStart={0}
+                loadMore={() => nextUrl && setUrl(nextUrl.replace(host, ''))}
+                hasMore={!is_loading && (nextUrl ? true : false)}
+                loader={<div className="loader" key={0}>Loading ...</div>}
+                useWindow={false}
+            >
+                <List
+                    bordered
+                    dataSource={listData}
+                    renderItem={item => (
+                        <List.Item>
+                          <div>
+                            <Avatar icon={"F"}/>&nbsp;&nbsp;
+                            <Typography.Text>Facebook User</Typography.Text>
+                          </div>
+                          <Button type="primary" className="messenger-btn" disabled>
+                            <svg className="messenger-icon" width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd"><path d="M12 0c-6.627 0-12 4.975-12 11.111 0 3.497 1.745 6.616 4.472 8.652v4.237l4.086-2.242c1.09.301 2.246.464 3.442.464 6.627 0 12-4.974 12-11.111 0-6.136-5.373-11.111-12-11.111zm1.193 14.963l-3.056-3.259-5.963 3.259 6.559-6.963 3.13 3.259 5.889-3.259-6.559 6.963z"/></svg>
+                            Messenger
+                          </Button>
+                        </List.Item>
+                    )}
+                />
+            </InfiniteScroll>
+        </React.Fragment>
+    );
 };
 
 export default InterestedVolunteers;
