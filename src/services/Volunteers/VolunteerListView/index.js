b8719f75930c7b03b5317e4b344e14632c2a20dc
Masum <mehedi.masum@newscred.com>
2020-05-27 01:37:26 +0600

    Update volunteers page

--- src/services/Volunteers/VolunteerListView/index.js
+++ src/services/Volunteers/VolunteerListView/index.js
@@ -1,16 +1,28 @@
 import React, {useEffect, useMemo, useState, useCallback} from 'react';
 import { useFetch } from '../../../hooks';
-import { Table, Space, Card, Layout } from 'antd';
-import { Avatar } from 'antd';
-import { Typography } from 'antd';
-import { UserOutlined } from '@ant-design/icons';
+import {Table, Space, Card, Layout, Button, Avatar, Typography} from 'antd';
+import {formatTime} from "../../../utils";
 const { Content } = Layout;
 
 
 const columns = [
   {
-    title: 'Facebook user id',
-    dataIndex: 'facebook_user_id',
+    title: 'Name',
+    render: (text, record) => (
+        <div>
+          <Avatar icon={"F"}/>&nbsp;&nbsp;
+          <Typography.Text>Facebook User</Typography.Text>
+        </div>
+    )
+  },
+  {
+    title: 'Actions',
+    render: (text, record) => (
+        <Button type="primary" className="messenger-btn">
+          <svg className="messenger-icon" width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd"><path d="M12 0c-6.627 0-12 4.975-12 11.111 0 3.497 1.745 6.616 4.472 8.652v4.237l4.086-2.242c1.09.301 2.246.464 3.442.464 6.627 0 12-4.974 12-11.111 0-6.136-5.373-11.111-12-11.111zm1.193 14.963l-3.056-3.259-5.963 3.259 6.559-6.963 3.13 3.259 5.889-3.259-6.559 6.963z"/></svg>
+          Messenger
+        </Button>
+    )
   },
 ];
 
@@ -39,7 +51,7 @@ const VolunteerListView = props => {
   return (
     <React.Fragment>
       <Content className="center-content">
-        <Card title="Volunteers" style={{width: '50%'}}>
+        <Card title="Connected Volunteers">
           <Table
             columns={columns}
             dataSource={tableData}
