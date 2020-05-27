afe075f526f7175b7d40e1206281e4a7388c818c
shakil ahmed <shakil.austcse27@gmail.com>
2020-05-27 12:19:29 +0600

    modal to send notification

--- src/services/Posts/PostDetailsView/index.js
+++ src/services/Posts/PostDetailsView/index.js
@@ -1,9 +1,9 @@
-import React, {useMemo} from 'react';
+import React, {useMemo, useState} from 'react';
 import Template from '../../../template';
 import {useParams} from "react-router-dom";
 import {useFetch} from '../../../hooks';
 import {Avatar, Badge, Button, Card, Space} from 'antd';
-import {Alert, Typography} from 'antd';
+import {Alert, Typography, Modal, Input} from 'antd';
 import { Descriptions } from 'antd';
 import {
     LinkOutlined,
@@ -17,17 +17,25 @@ import { Tag } from 'antd';
 import { Checkbox } from 'antd';
 
 const {Content} = Layout;
+const {TextArea} = Input;
 
 const PostEditView = props => {
     const {id} = useParams();
     const [post_response, , , , status] = useFetch(`/api/voluntree/posts/${id}`);
     const [interest_response, , setUrl] = useFetch(`/api/voluntree/posts/${id}/interests/`);
-
+    const [showModal, setShowModal] = useState(false);
+    const [modalValue, setModalValue] = useState('');
     const post = useMemo(() => {
         if (!post_response) return {};
         return post_response;
     }, [post_response]);
 
+    const onModalOk = () => {
+        // write code
+        setModalValue('');
+        setShowModal(false);
+    }
+ 
     return (
         <React.Fragment>
             <PageHeader
@@ -68,7 +76,8 @@ const PostEditView = props => {
             <Row gutter={16}>
                 <Col span={12}>
                     <Card title="Updates" extra={
-                        <Button type="primary" className="messenger-btn">
+                        <Button type="primary" className="messenger-btn" onClick={() => setShowModal(!showModal)}>
+                            <svg className="messenger-icon" width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd"><path d="M12 0c-6.627 0-12 4.975-12 11.111 0 3.497 1.745 6.616 4.472 8.652v4.237l4.086-2.242c1.09.301 2.246.464 3.442.464 6.627 0 12-4.974 12-11.111 0-6.136-5.373-11.111-12-11.111zm1.193 14.963l-3.056-3.259-5.963 3.259 6.559-6.963 3.13 3.259 5.889-3.259-6.559 6.963z"/></svg>
                             Send new update
                         </Button>
                     }>
@@ -81,7 +90,22 @@ const PostEditView = props => {
                     </Card>
                 </Col>
             </Row>
-
+            
+            <div>
+            <Modal
+                title="Message to All Volunteers"
+                visible={showModal}
+                onOk={onModalOk}
+                onCancel={() => setShowModal(false)}
+                >
+                <TextArea
+                    value={modalValue}
+                    onChange={e => setModalValue(e.target.value)}
+                    placeholder="Controlled autosize"
+                    autoSize={{ minRows: 3, maxRows: 5 }}
+                />
+                </Modal>
+            </div>
         </React.Fragment>
     );
 };
