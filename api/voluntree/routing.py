9abd3db5a2a01f01d51ee8de6170ea3fd3a8dec8
tanvir <tanvir.hasan@codemarshal.com>
2020-05-29 22:18:50 +0600

    fix(api): fix websocket endpont route

--- api/voluntree/routing.py
+++ api/voluntree/routing.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import consumers
 
 websocket_urlpatterns = [
-    path('ws/voluntree/posts/<slug:post_id>/interests',
+    path('ws/voluntree/posts/<uuid:post_id>/interests',
          consumers.PostInterestConsumer),
 ]
