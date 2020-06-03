35f3e3e9bc1b36dad15dbfde42f4e9acdf4de851
Masum <mehedi.masum@newscred.com>
2020-06-04 02:09:12 +0600

    Add NLP and VMS settings UI

--- src/services/Settings/index.js
+++ src/services/Settings/index.js
@@ -1,43 +1,68 @@
 import './index.css';
 
 import React from 'react';
-import {Card, Button} from 'antd';
+import {Button, Card} from 'antd';
 import {useFetch} from '../../hooks';
 import PageListView from './PageListView';
 import FormLayoutDemo from "./Webhooks";
+import WitSettingsForm from "./WitSettings";
+
+import nationbuilder from '../../assets/icons/nationbuilder.svg';
 
 
 const Settings = () => {
-    const [oauth_url] = useFetch('/api/facebook/oauth_url/');
+  const [oauth_url] = useFetch('/api/facebook/oauth_url/');
 
-    const onClickConnect = () => {
-        window.open(oauth_url, "Popup", "width=800,height=800");
-    };
+  const onClickConnect = () => {
+    window.open(oauth_url, "Popup", "width=800,height=800");
+  };
 
-    return (
-        <div>
-            <div className="create-new-page">
-              <Button
-                type="primary"
-                disabled={!oauth_url}
-                onClick={onClickConnect}
-              >
-                Connect Facebook Pages
-              </Button>
-            </div>
-            <Card title="Connected Pages">
-                <PageListView/>
-            </Card>
+  return (
+    <div>
+      <div className="create-new-page">
+        <Button
+          type="primary"
+          disabled={!oauth_url}
+          onClick={onClickConnect}
+        >
+          Connect Facebook Pages
+        </Button>
+      </div>
 
-            <br/>
+      <Card title="Page Settings">
+        <WitSettingsForm readOnly={false}/>
+      </Card>
+      <br/>
 
-            <div>
-                <Card title="Webhooks">
-                    <FormLayoutDemo/>
-                </Card>
-            </div>
-        </div>
-    );
+      <Card title="Connected Pages">
+        <PageListView/>
+      </Card>
+      <br/>
+
+      <Card title="Messenger Webhooks">
+        <FormLayoutDemo/>
+      </Card>
+      <br/>
+
+      <Card title="VMS Integrations">
+        <Card
+          hoverable
+          className="vms-card"
+          cover={<img alt="example" src={nationbuilder} className="vms-nb"/>}
+        >
+          <div className="vms-card-body">
+            <Button
+              type="primary"
+              className="vms-connect"
+            >
+              Connect Nation Builder
+            </Button>
+          </div>
+        </Card>
+
+      </Card>
+    </div>
+  );
 };
 
 export default Settings;
