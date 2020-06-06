3b739fd648a40e9e430ed26fd46263f65d82e4a6
Shakil Ahmed <shakil@codemarshal.com>
2020-06-07 02:37:05 +0600

    frontend integration

--- src/services/Posts/PostCreateView/index.js
+++ src/services/Posts/PostCreateView/index.js
@@ -7,6 +7,7 @@ import PostFrom from '../Form';
 const PostCreateView = () => {
   const history = useHistory();
   const [pages_response] = useFetch('/api/pages/?limit=100');
+  const [signup_response] = useFetch('/api/signups/');
   const [errorMsg, setErrorMsg] = useState(null);
   const [loading, setLoading] = useState(false);
 
@@ -40,13 +41,18 @@ const PostCreateView = () => {
     if(!pages_response) return [];
     return pages_response.results;
   }, [pages_response]);
+  
+  const signups = useMemo(() => {
+    if(!signup_response) return [];
+    return signup_response.results;
+  }, [signup_response]);
 
   return (
     <React.Fragment>
       <Layout.Content className="center-content">
         <Card title="Create New Post">
           {errorMsg && <Alert message={errorMsg} type="error" banner closable/> }
-          <PostFrom onSubmit={onSubmit} pages={pages} loading={loading}/>
+          <PostFrom onSubmit={onSubmit} pages={pages} signups={signups} loading={loading}/>
         </Card>
       </Layout.Content>
     </React.Fragment>
