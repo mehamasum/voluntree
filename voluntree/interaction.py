42384062e2176d3d77cdeeba678f7ca105f6c967
Masum <mehedi.masum@newscred.com>
2020-06-21 00:04:16 +0600

    Use common method

--- voluntree/interaction.py
+++ voluntree/interaction.py
@@ -332,97 +332,66 @@ class InteractionHandler:
 
         elif InteractionHandler.validate_email(text):
             email = text
-            context = InteractionHandler.get_context(psid, page_id)
-            if not context:
-                return InteractionHandler.handle_expired_session(psid, page_id)
-
-            post_instance = Post.objects.get(facebook_post_id=context['post_id'])
-            VolunteerService.send_verification_email(psid, page_id, post_instance.id, email)
-
-            InteractionHandler.set_context(psid, page_id, {
-                'post_id': context['post_id'],
-                'state': InteractionHandler.ASKED_FOR_PIN,
-                'email': email
-            })
-            InteractionHandler.send_reply(psid, page_id, {
-                'text': 'What is the OTP?'
-            })
+            InteractionHandler.handle_email(psid, page_id, email)
 
         elif email_entity and InteractionHandler.validate_email(email_entity['value']) and email_entity['confidence'] > 0.8:
             email = email_entity['value']
-            context = InteractionHandler.get_context(psid, page_id)
-            if not context:
-                return InteractionHandler.handle_expired_session(psid, page_id)
-
-            post_instance = Post.objects.get(facebook_post_id=context['post_id'])
-            VolunteerService.send_verification_email(psid, page_id, post_instance.id, email)
-
-            InteractionHandler.set_context(psid, page_id, {
-                'post_id': context['post_id'],
-                'state': InteractionHandler.ASKED_FOR_PIN,
-                'email': email
-            })
-            InteractionHandler.send_reply(psid, page_id, {
-                'text': 'What is the OTP?'
-            })
+            InteractionHandler.handle_email(psid, page_id, email)
 
         elif InteractionHandler.validate_otp_str(text):
             pin = text
-
-            context = InteractionHandler.get_context(psid, page_id)
-            if not context:
-                return InteractionHandler.handle_expired_session(psid, page_id)
-
-            post_id = context['post_id']
-            email = context['email']
-
-            res = VolunteerService.verify_volunteer(psid, page_id, email, int(pin))
-            print('got res', res)
-            if not res:
-                # TODO: handle wrong attempt
-                pass
-
-            volunteer = Volunteer.objects.get(facebook_user_id=psid, facebook_page_id=page_id)
-            post = Post.objects.get(facebook_post_id=post_id)
-
-            if NationBuilderService.create_people(email, volunteer, post):
-                InteractionHandler.send_reply(psid, page_id, {
-                    'text': 'Your email is verified. ' +
-                    'We have created an account for you in our volunteer management software.'
-                })
-
-            InteractionHandler.reply_with_slot_picker(psid, page_id, post)
-            InteractionHandler.reset_context(psid, page_id)
+            InteractionHandler.handle_pin(psid, page_id, pin)
 
         elif otp_entity and InteractionHandler.validate_otp(otp_entity['value']) and otp_entity['confidence'] > 0.7:
             pin = otp_entity['value']
-
-            context = InteractionHandler.get_context(psid, page_id)
-            if not context:
-                return InteractionHandler.handle_expired_session(psid, page_id)
-
-            post_id = context['post_id']
-            email = context['email']
-
-            res = VolunteerService.verify_volunteer(psid, page_id, email, int(pin))
-            print('got res', res)
-            if not res:
-                # TODO: handle wrong attempt
-                pass
-
-            volunteer = Volunteer.objects.get(facebook_user_id=psid, facebook_page_id=page_id)
-            post = Post.objects.get(facebook_post_id=post_id)
-
-            if NationBuilderService.create_people(email, volunteer, post):
-                InteractionHandler.send_reply(psid, page_id, {
-                    'text': 'Your email is verified. ' +
-                    'We have created an account for you in our volunteer management software.'
-                })
-
-            InteractionHandler.reply_with_slot_picker(psid, page_id, post)
-            InteractionHandler.reset_context(psid, page_id)
+            InteractionHandler.handle_pin(psid, page_id, pin)
 
         return Response(status.HTTP_200_OK)
+
+    @staticmethod
+    def handle_email(psid, page_id, email):
+        context = InteractionHandler.get_context(psid, page_id)
+        if not context:
+            return InteractionHandler.handle_expired_session(psid, page_id)
+
+        post_instance = Post.objects.get(facebook_post_id=context['post_id'])
+        VolunteerService.send_verification_email(psid, page_id, post_instance.id, email)
+
+        InteractionHandler.set_context(psid, page_id, {
+            'post_id': context['post_id'],
+            'state': InteractionHandler.ASKED_FOR_PIN,
+            'email': email
+        })
+        InteractionHandler.send_reply(psid, page_id, {
+            'text': 'What is the OTP?'
+        })
+
+    @staticmethod
+    def handle_pin(psid, page_id, pin):
+        context = InteractionHandler.get_context(psid, page_id)
+        if not context:
+            return InteractionHandler.handle_expired_session(psid, page_id)
+
+        post_id = context['post_id']
+        email = context['email']
+
+        res = VolunteerService.verify_volunteer(psid, page_id, email, int(pin))
+        print('got res', res)
+        if not res:
+            # TODO: handle wrong attempt
+            pass
+
+        volunteer = Volunteer.objects.get(facebook_user_id=psid, facebook_page_id=page_id)
+        post = Post.objects.get(facebook_post_id=post_id)
+
+        if NationBuilderService.create_people(email, volunteer, post):
+            InteractionHandler.send_reply(psid, page_id, {
+                'text': 'Your email is verified. ' +
+                        'We have created an account for you in our volunteer management software.'
+            })
+
+        InteractionHandler.reply_with_slot_picker(psid, page_id, post)
+        InteractionHandler.reset_context(psid, page_id)
 
     @staticmethod
     def reply_with_slot_picker(psid, page_id, post):
