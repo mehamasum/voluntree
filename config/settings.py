890d33c6047fdd1497b4e1562dec0decfca03171
Masum <mehedi.masum@newscred.com>
2020-07-14 22:55:27 +0600

    Remove wrong static dir

--- config/settings.py
+++ config/settings.py
@@ -304,5 +304,3 @@ if USE_S3:
     PUBLIC_MEDIA_LOCATION = 'media'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/'
     DEFAULT_FILE_STORAGE = 'config.storage.PublicMediaStorage'
-
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
