0578158cdfdf7e1d09ce83a643f2c4d9904e0a6d
Masum <mehedi.masum@newscred.com>
2020-07-23 00:25:30 +0600

    Inforce s3 url to remove auth strings
    Based on: https://github.com/jschneier/django-storages/issues/254

--- config/settings.py
+++ config/settings.py
@@ -302,6 +302,7 @@ if USE_S3:
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     AWS_QUERYSTRING_AUTH = False
     AWS_LOCATION = 'local' if DEBUG else 'production'
+    AWS_S3_CUSTOM_DOMAIN = "%s.s3.amazonaws.com" % AWS_STORAGE_BUCKET_NAME
 
 USE_FILE_LOGGER = env.bool('USE_FILE_LOGGER', default=False)
 log_handlers = {
